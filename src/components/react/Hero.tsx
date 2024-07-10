108d4185d3fcd4b23f19efd7d04106ed5d8fcdba
naineel1209 <soyantarnaineel@gmail.com>
2024-07-10 23:42:39 +0530

    completed hero component, started with the articles-box

--- src/components/react/Hero.tsx
+++ src/components/react/Hero.tsx
@@ -1,59 +1,78 @@
-import Swiper from 'swiper';
-import {Navigation, Mousewheel, Autoplay } from 'swiper/modules'
-import {Image} from 'astro:assets';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel, Autoplay, Pagination } from 'swiper/modules'
 
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import 'swiper/css/mousewheel';
 import 'swiper/css/autoplay';
-
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Mousewheel, Autoplay],
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    mousewheel: true,
-    autoplay: {
-        delay: 5000,
-    },
-    loop: true,
-});
+import { useEffect } from 'react';
 
 const Hero = () => {
-
     const categories = [
         {
             id: 1,
-            name: 'Tech Blogs'
+            name: 'Tech Blogs',
+            bgColor: 'bg-blue-500' // Tailwind CSS background color class
         },
         {
             id: 2,
-            name: 'Tech Tutorials'
+            name: 'Tech Tutorials',
+            bgColor: 'bg-green-500'
         },
         {
             id: 3,
-            name: 'Code Snippets'
+            name: 'Code Snippets',
+            bgColor: 'bg-red-500'
         },
         {
             id: 4,
-            name: 'Projects'
+            name: 'Projects',
+            bgColor: 'bg-yellow-500'
         },
-    ]
+    ];
 
     return (
-        <div className={'swiper border-2'}>
-            <div className="swiper-wrapper">
-                {/*// <!-- Slides -->*/}
-                <div className="swiper-slide">Slide 1</div>
-                <div className="swiper-slide">Slide 2</div>
-                <div className="swiper-slide">Slide 3</div>
-                ...
-            </div>
+        <div className={'swiper border-2 mb-[3rem]'}>
+            <style>
+                {`@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
 
-
-            <div className="swiper-button-prev"></div>
-            <div className="swiper-button-next"></div>
+                .heading {
+                    font - family: "Montserrat", sans-serif;
+                font-weight: 900;
+                font-optical-sizing: auto;
+                font-style: normal;
+                letter-spacing: 0.2rem;
+                text-transform: uppercase;
+                }`
+                }
+            </style>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                grabCursor={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination, Autoplay, Mousewheel]}
+                className="mySwiper"
+                loop={true}
+                autoplay={{
+                    delay: 2000
+                }}
+                speed={500}
+                mousewheel={true}
+                style={{ height: '100%' }}
+            >
+                {
+                    categories.map((category) => {
+                        return (
+                            <SwiperSlide key={category.id} className={`swiper-slide justify-center p-[2rem] heading ${category.bgColor} rounded-lg text-center`}>
+                                <span className="text-white text-[5rem]">{category.name}</span>
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
         </div>
     )
 }
