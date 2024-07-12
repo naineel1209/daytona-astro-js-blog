b421a0f31896f04b6e1d46d0ece93758856d79f3
Naineel Soyantar <soyantarnaineel@gmail.com>
2024-07-12 17:19:52 +0530

    started with ssr blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,18 @@
 import { defineCollection, z } from "astro:content";
 
-const blogCollection = defineCollection({
+const blogsCollection = defineCollection({
   type: "content",
-  schema: {
+  schema: z.object({
     title: z.string(),
     description: z.string(),
     author: z.string(),
-    date: z.date(),
+    date: z.string().date(),
     image: z.string().optional(),
     tags: z.array(z.string()),
-    slug: z.string().optional(),
-  }
+    draft: z.boolean().default(false)
+  })
 });
 
 export const collections = {
-  blogs: blogCollection,
+  blogs: blogsCollection,
 };
