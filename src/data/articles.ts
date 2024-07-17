0c42bf90e93d29b67c6007d9d534162aaec2145b
naineel1209 <soyantarnaineel@gmail.com>
2024-07-17 23:47:59 +0530

    completed search functionality using sanity, started with solo article page

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -2,8 +2,8 @@ import { getCollection, getEntryBySlug } from "astro:content";
 // @ts-ignore
 import { sanityClient } from 'sanity:client';
 import { ARTICLES_PER_PAGE, LATEST_ARTICLE_LIMIT, STARRED_ARTICLE_LIMIT } from "../constants";
-import type { TArticles } from "../types/types";
-import { getArticlesQuery, getLatestArticlesQuery, getStarredArticleQuery } from "./query/articles.query";
+import type { TArticle, TArticles } from "../types/types";
+import { getArticleBySlugQuery, getArticlesBySearchQuery, getArticlesQuery, getLatestArticlesQuery, getStarredArticleQuery } from "./query/articles.query";
 
 export const getArticles = async (limit?: number) => {
   let articles = await getCollection("blogs");
@@ -44,6 +44,25 @@ export const getLatestSanityArticles = async (): Promise<TArticles[]> => {
   const sanityArticles: TArticles[] = await sanityClient.fetch(getLatestArticlesQuery(limit));
 
   return sanityArticles;
+}
+
+export const getSanityArticleBySlug = async (slug: string): Promise<TArticle> => {
+  return await sanityClient.fetch(getArticleBySlugQuery(slug));
+}
+
+export const getSanityArticlesBySearch = async (search: string, limit?: number, page?: number): Promise<TArticles[]> => {
+  if (!limit || limit < 1) {
+    limit = ARTICLES_PER_PAGE;
+  }
+
+  if (!page || page < 1) {
+    page = 1;
+  }
+
+  const sanityArticles: TArticles[] = await sanityClient.fetch(getArticlesBySearchQuery(search, limit, page));
+
+  return sanityArticles;
+
 }
 
 export const getArticle = async (slug: string) => {
--- src/data/query/articles.query.ts
+++ src/data/query/articles.query.ts
@@ -37,3 +37,34 @@ export const getStarredArticleQuery = (limit: number) => (`
         draft
     }
 `)
+
+export const getArticleBySlugQuery = (slug: string) => (`
+    *[_type == "article" && slug.current == "${slug}" && draft == false]{
+        _id,
+        title,
+        description,
+        author,
+        date,
+        image,
+        tags,
+        slug,
+        starred,
+        draft,
+        content
+    }[0]
+`)
+
+export const getArticlesBySearchQuery = (search: string, limit: number, page: number) => (`
+    *[_type == "article" && draft == false && title match "${search}*" || description match "${search}*" || author match "${search}*" || tags[] match "${search}*"] | order(date desc){
+        _id,
+        title,
+        description,
+        author,
+        date,
+        image,
+        tags,
+        slug,
+        starred,
+        draft
+    }[${(page - 1) * limit}...${page * limit}]
+`);
