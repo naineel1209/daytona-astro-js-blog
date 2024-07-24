21d2154cfbf21adfb2f68a5b7dd36da1ead6400b
Naineel Soyantar <soyantarnaineel@gmail.com>
2024-07-24 10:26:30 +0530

    done-dash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
+import { loadEnv } from "vite";
 import sanity from "@sanity/astro";
+const { SECRET_SANITY_TOKEN, SECRET_SANITY_PROJECT_ID, SECRET_SANITY_DATASET } = loadEnv("", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sanity({
-    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-    dataset: import.meta.env.VITE_SANITY_DATASET,
+    projectId: SECRET_SANITY_PROJECT_ID,
+    dataset: SECRET_SANITY_DATASET,
     apiVersion: '2022-07-03',
     useCdn: true, //for SSR - true
     studioBasePath: '/admin',
-    token: import.meta.env.VITE_SANITY_TOKEN,
+    token: SECRET_SANITY_TOKEN,
   })],
   output: "server",
   adapter: vercel()
