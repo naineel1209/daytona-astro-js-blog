1a4c6bc9e83e2a6735b53d6bd450885409659399
Naineel Soyantar <soyantarnaineel@gmail.com>
2024-07-29 11:41:43 +0530

    removed netlify and added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
 import { defineConfig } from 'astro/config';
 import { loadEnv } from "vite";
-import netlify from "@astrojs/netlify";
-const { SECRET_SANITY_TOKEN, SECRET_SANITY_PROJECT_ID, SECRET_SANITY_DATASET } = loadEnv(process.env.NODE_ENV
-  , process.cwd(), "");
+import vercel from "@astrojs/vercel/serverless";
+const {
+  SECRET_SANITY_TOKEN,
+  SECRET_SANITY_PROJECT_ID,
+  SECRET_SANITY_DATASET
+} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,10 +17,16 @@ export default defineConfig({
     projectId: process.env.SECRET_SANITY_PROJECT_ID || SECRET_SANITY_PROJECT_ID,
     dataset: process.env.SECRET_SANITY_DATASET || SECRET_SANITY_DATASET,
     apiVersion: '2022-07-03',
-    useCdn: true, //for SSR - true
+    useCdn: true,
+    //for SSR - true
     studioBasePath: '/admin',
-    token: process.env.SECRET_SANITY_DATASET || SECRET_SANITY_TOKEN,
+    token: process.env.SECRET_SANITY_DATASET || SECRET_SANITY_TOKEN
   })],
   output: "server",
-  adapter: netlify()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  })
 });
